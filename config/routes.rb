81475c067583d45e94e14e28de2db9e06ee5d95f
Rymndkraft <mstamaria.a12241624@umak.edu.ph>
2026-01-31 13:47:43 +0800

    login page initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "pages#home"
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
+  get "register", to: "registrations#new"
+  post "register", to: "registrations#create"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
