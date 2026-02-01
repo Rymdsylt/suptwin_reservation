e7c48b066dc80eef5d8ea695a10696ffa1234ac9
Rymndkraft <mstamaria.a12241624@umak.edu.ph>
2026-02-01 15:53:10 +0800

    more migration and optimization
    add indexes, email uniqueness and smallint columns

--- db/migrate/20260201000001_optimize_column_types.rb
+++ db/migrate/20260201000001_optimize_column_types.rb
@@ -4,6 +4,10 @@ class OptimizeColumnTypes < ActiveRecord::Migration[8.1]
     change_column :tables, :capacity, 'smallint'
     change_column :reservations, :party_size, 'smallint'
     change_column :reservations, :status, 'smallint'
+    add_index :reservations, :date
+    add_index :reservations, [:date, :time_slot_id, :status]
+    add_index :reservations, :status
+    add_index :users, :email, unique: true
   end
 end
 #dont run
