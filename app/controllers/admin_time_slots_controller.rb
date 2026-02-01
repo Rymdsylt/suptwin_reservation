d35e1966ea0f54b28f6e949f55b1f33ee95e2e85
Rymndkraft <mstamaria.a12241624@umak.edu.ph>
2026-02-02 00:41:07 +0800

    fix

--- app/controllers/admin_time_slots_controller.rb
+++ app/controllers/admin_time_slots_controller.rb
@@ -40,6 +40,6 @@ class AdminTimeSlotsController < ApplicationController
   private
 
   def time_slot_params
-    params.require(:time_slot).permit(:start_time, :end_time)
+    params.require(:time_slot).permit(:day, :start_time, :end_time)
   end
 end
